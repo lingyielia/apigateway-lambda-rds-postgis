410ca47816112622d104c2c55acd769b7d147a03
Lingyi Zhang <31973683+lingyielia@users.noreply.github.com>
2018-03-29 23:33:59 -0400

    python version

--- lambda_function_python/lambda_function.py
+++ lambda_function_python/lambda_function.py
@@ -1,24 +1,50 @@
 from datetime import datetime
 import os
 import psycopg2
+from botocore.vendored import requests
+import json
 
 con = None
-#query = ''
 
 def lambda_handler(event, context):
-    print('Scheduled maintenance started at {}'.format(str(datetime.now())))
+    querytext = ("SELECT time::timestamp AS recenttime, " +
+                "ST_X(ST_ClosestPoint(the_geom, ST_GeomFromText('Point(" + str(event['lon']) + " " + str(event['lon']) + ")', 4326))) AS pointLon, " +
+                "ST_Y(ST_ClosestPoint(the_geom, ST_GeomFromText('Point(" + str(event['lon']) + " " + str(event['lon']) + ")', 4326))) AS pointLat, " +
+                "device " +
+                "FROM testtable1 " +
+                "WHERE route='" + event['route'] + "' "
+                "ORDER BY time DESC " +
+                "LIMIT 1;")
+
+    print('Time of user query: {}'.format(str(datetime.now())))
     print(event)
+
     try:
-        host = "businstance.c4lkxc6noi6m.us-east-1.rds.amazonaws.com"
-        database = "busdb"
-        user = "testrole"
-        password = "argolabs"
+        host = "****"
+        database = "****"
+        user = "****"
+        password = "****"
 
         con = psycopg2.connect(host=host, database=database, user=user, password=password)
         cur = con.cursor()
-        cur.execute('SELECT * FROM devicelocation')
+        cur.execute(querytext)
         rows = cur.fetchall()
         print(rows)
+        back_time = rows[0][0]
+        back_lon = str(rows[0][1])
+        back_lat = str(rows[0][2])
+        back_device = rows[0][3]
+        #print("This location information was in database " + str(datetime.now()-back_time) + " before.")
+
+        API_KEY = 'yourkey'
+        url = ('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&' +
+              'origins=' + str(event['lat']) + ',' + str(event['lon']) + '&destinations=' +
+               str(back_lat) + ',' + str(back_lon) + '&key=' + str(API_KEY))
+
+        req = requests.get(url)
+        time_result = req.json()['rows'][0]['elements'][0]['duration']['text']
+        return {'expected_time': time_result, 'device': back_device}
+        #return event
 
 
     except psycopg2.DatabaseError as e:
